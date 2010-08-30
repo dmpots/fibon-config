9ff4bd7202804bc25e80552a3c99d2af77b63b6e
David M Peixotto <dmp@rice.edu>
2010-08-30 13:26:14 -0500

    Config file for ghc 6.10

---
+++ Ghc610_Config.hs
@@ -0,0 +1,32 @@
+module Ghc610_Config(
+  config
+)
+where
+import Fibon.Run.Config
+
+config :: RunConfig
+config = RunConfig {
+    configId = "ghc610"
+  , runList  = map RunSingle $ filter (/= Gf) allBenchmarks
+  , sizeList = [Test, Ref]
+  , tuneList = [Base, Peak]
+  , iterations = 10
+  , flagsBuilder = flags
+  }
+
+flags :: FlagBuilder
+flags (ConfigTune Base) ConfigBenchDefault = do
+  append ConfigureFlags "--disable-optimization"
+
+flags (ConfigTune Base) (ConfigBench Palindromes) = do
+  append RunFlags "+RTS -K256M -RTS"
+
+flags (ConfigTune Base) (ConfigBench TernaryTrees) = do
+  append RunFlags "+RTS -K16M -RTS"
+
+flags (ConfigTune Peak) ConfigBenchDefault = do
+  append ConfigureFlags "--enable-optimization=2"
+
+flags _ _ = do
+  done
+
