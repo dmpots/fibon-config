7b7675543a74f75781d17fa407998d62163f78ac
David M Peixotto <dmp@rice.edu>
2011-02-02 17:38:22 -0600

    Use -O2 for both Base and Peak dynamorio run

--- Ghc612Dynamo_Config.hs
+++ Ghc612Dynamo_Config.hs
@@ -15,8 +15,13 @@ dr :: FilePath
 dr = "/home/dave/dr/bin/drrun"
 
 build :: ConfigBuilder
-build t b = do
+build t@(ConfigTune Peak) b = do
   mbOpts <- getEnv "DR_OPTS"
   let opts = maybe "" id mbOpts
   useRunScript dr opts
   (configBuilder Ghc612.config) t b
+
+build (ConfigTune Base) ConfigBenchDefault = do
+  append ConfigureFlags "--enable-optimization=2"
+
+build t b = (configBuilder Ghc612.config) t b
